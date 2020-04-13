3bb35b6cc51c24ca6992969f2e86182f23e4ee0c
DRJ28 <dheerajmehta10275@gmail.com>
2020-04-13 12:04:03 +0530

    new ap

---
+++ precache-manifest.bc510147f5554619e295de9c49e36bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968f80737b28443b590c1c659674c752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16f60220df5a00007d2e",
+    "url": "/static/css/main.7add19d1.chunk.css"
+  },
+  {
+    "revision": "7b7cffaeebbc759c5931",
+    "url": "/static/js/2.1521d519.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1521d519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f60220df5a00007d2e",
+    "url": "/static/js/main.82a4ab78.chunk.js"
+  },
+  {
+    "revision": "db9f067e02a9ca36140d",
+    "url": "/static/js/runtime-main.37884cf8.js"
+  }
+]);
